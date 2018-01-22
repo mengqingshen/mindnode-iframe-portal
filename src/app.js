a11481d1680d9e79071437ce8ab97b5b46a6091a
孟庆申 <mengqingshen@meituan.com>
2018-01-22 14:57:23 +0800

    the last version using nodejs only isn't working well, so fix it with koa

--- src/app.js
+++ src/app.js
@@ -1,41 +1,15 @@
-const https = require('https')
-const http = require('http')
-const url = require('url')
-const queryString = require('querystring')
+const Koa = require('koa')
+const proxyMiddleware = require('http-proxy-middleware')
+const c2k = require('koa-connect')
 
 const PORT = 9000
+const app = new Koa()
 
-
-const app = http.createServer((req, res) => {
-  const parts = req.url.split('?')
-  if (parts.length > 0) {
-    const querysStr = parts.pop()
-    const querys = queryString.parse(querysStr, '&', '=')
-    if (querys.url) {
-      const urlObj = url.parse(querys.url)
-      const sreq = https.request({
-        hostname: urlObj.hostname,
-        path: urlObj.path,
-        hash: urlObj.hash,
-        method: 'get'
-      }, (sres) => {
-        sres.pipe(res)
-        sres.on('end', () => { console.log('done') })
-      })
-      sreq.end()
-    } else {
-      const sreq = https.request({
-        hostname: 'my.mindnode.com',
-        path: req.url,
-        method: req.method
-      }, (sres) => {
-        sres.pipe(res)
-        sres.on('end', () => { console.log('done') })
-      })
-      sreq.end()
-    }
-  }
-})
+app.use(c2k(proxyMiddleware(['**/*'], {
+  target: 'https://my.mindnode.com',
+  changeOrigin: true,
+  logLevel: 'debug'
+})))
 
 app.listen(PORT)
 console.log(`server started on ${PORT}`)
